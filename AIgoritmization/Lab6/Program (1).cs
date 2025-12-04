a7d8345ed7c990d9eba550ca257ec8c4f66c5118
Oopmie <oopmie@mail.ru>
2025-12-04 22:10:54 +0200

    Add files via upload

--- AIgoritmization/Lab6/Program (1).cs
+++ AIgoritmization/Lab6/Program (1).cs
@@ -1,4 +1,28 @@
 ﻿using Lab6_library;
+//функции. высокий. вариант 11
+
+Console.Write("Введите первое двоичное число: ");
+string a = Console.ReadLine();
+
+Console.Write("Введите второе двоичное число: ");
+string b = Console.ReadLine();
+
+int aInt = Convert.ToInt32(a, 2);
+int bInt = Convert.ToInt32(b, 2);
+
+string result = AddBinary(aInt, bInt);
+
+Console.WriteLine($"Сумма = {result}");
+
+static string AddBinary(int a, int b)
+{
+    if (b == 0)
+        return Convert.ToString(a, 2);
+    int sum = a ^ b;
+    int carry = (a & b) << 1;
+    return AddBinary(sum, carry);
+}
+
 //библиотеки. вариант 11. средний уровень
 
 double[,] myMas = new double[6, 6];
@@ -6,8 +30,8 @@ for (int i = 0; i < myMas.GetLength(0); i++)
 {
     for (int j = 0; j < myMas.GetLength(1); j++)
     {
-        myMas[i, j] = (Math.Pow(Math.Pow(Math.Log(i+1),2)/(3.5-0.5*j),2)+Math.Pow((-Math.E),(i+j)));
-        Console.Write($"{myMas[i,j]:F2} ");
+        myMas[i, j] = (Math.Pow(Math.Pow(Math.Log(i + 1), 2) / (3.5 - 0.5 * j), 2) + Math.Pow((-Math.E), (i + j)));
+        Console.Write($"{myMas[i, j]:F2} ");
     }
     Console.WriteLine();
 }
