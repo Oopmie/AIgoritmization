152c7d69f43b18212ca76ad668886d05aec6fe07
Oopmie <oopmie@mail.ru>
2025-12-05 11:48:22 +0200

    Add files via upload

---
+++ AIgoritmization/Lab9/Program.cs
@@ -0,0 +1,36 @@
+﻿//инкапсуляция. вариант 29. средний
+Console.WriteLine("Введите длину комнаты");
+int length = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите ширину комнаты");
+int width = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите высоту комнаты");
+int height = int.Parse(Console.ReadLine()!);
+Room room = new Room(length, width, height);
+//Room.Print();
+class Room
+{
+    private double length;
+    private double width;
+    private double height;
+
+    public Room(double length, double width, double height)
+    {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+    public double Length
+    { get { return length; } }
+    public double Width
+    { get { return width; } }
+    public double Height
+    { get { return height; } }
+    public decimal getTotal()
+    { 
+
+    }
+    public void Print()
+    {
+        Console.WriteLine($"стоимость {time} минут разговора = {getTotal():F2} рублей");
+    }
+}
